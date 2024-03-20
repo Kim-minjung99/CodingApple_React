655b1ca3241f1ba6ff71346c4f327a59660f2a67
Kim-minjung99 <minjung5229@naver.com>
2024-03-21 00:30:48 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Modal from './components/Modal';
 
 function App() {
+
+  let [itemTitle, setItemTitle] = useState(["남자 옷", "남자 바지","화장품"]);
+  var [likes, setLikes] = useState([0,0,0]);
+  var [isOpenModal, setOpenModal] = useState(false);
+
+  function toggleModal(){
+    setOpenModal(!isOpenModal);
+  }
+
+  function sortItems(itemTitle : string[]){
+    const itemTitleCopy = [...itemTitle];
+    setItemTitle(itemTitleCopy.sort());
+  }
+
+  function countUpLikes(likes: number[], index: number){
+    const copyLikes = [...likes];
+    copyLikes[index] = copyLikes[index]+1;
+    setLikes(copyLikes);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        isOpenModal ===  true ?
+        <Modal></Modal> :
+        null
+      }
+      <div className='black-nav'>
+        <div>개발 blog</div>
+      </div>
+      <button onClick={()=>{
+        const itemTitleCopy = [...itemTitle];
+        itemTitleCopy[0] = "여자 옷";
+        setItemTitle(itemTitleCopy);
+      }}> 여자옷으로 변경하기 </button>
+      <button onClick={()=>{sortItems(itemTitle)}}>정렬하기</button>
+      {
+        itemTitle.map((titleElement, index)=>{
+          return(
+            <div className='list'>
+              <h4><span onClick={toggleModal}>{titleElement}</span>
+                <div>2월 17일 발행</div>
+                <button onClick={()=>{countUpLikes(likes, index)}}>♥</button>
+              </h4>
+              <span>{likes[index]}</span>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,8 @@
+function Modal(){
+    return(
+        <div className="modal">
+            모달입니다.
+        </div>
+    );
+}
+export default Modal;
